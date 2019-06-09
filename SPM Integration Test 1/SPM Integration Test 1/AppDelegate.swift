bae8e3b76a95f61bdb603f743567627ae7968d30
Sam Deane <sam@elegantchaos.com>
2019-06-09 12:03:39 +0100

    first commit

---
+++ SPM Integration Test 1/SPM Integration Test 1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SPM Integration Test 1
+//
+//  Created by Sam Deane on 08/06/2019.
+//  Copyright © 2019 Elegant Chaos Limited. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
